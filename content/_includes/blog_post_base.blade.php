4361d4bcd901079a09a6f127478e16e7d749bbe3
Mohamed Said <themohamedsaid@gmail.com>
2016-03-06 09:27:12 +0000

    Update

--- content/_includes/blog_post_base.blade.php
+++ content/_includes/blog_post_base.blade.php
@@ -8,4 +8,16 @@
 
     @yield('post_body')
 
+    <span class="post-footer">—————————————&nbsp&nbspThat's all folks&nbsp&nbsp—————————————</span>
+
+    <div class="text-center">
+        <a href="https://twitter.com/share"
+           class="twitter-share-button"
+           data-text="{{str_limit($__env->yieldContent('post::title'),95)}}"
+           data-via="themsaid">Tweet</a>
+        &nbsp&nbsp&nbsp
+        <a href="https://twitter.com/intent/tweet?screen_name=themsaid"
+           class="twitter-mention-button"
+           data-related="themsaid">Tweet to @themsaid</a>
+    </div>
 @stop
